b240c339c82cb58f79138b2e45e8c6371f13ce81
Emmanuel Nunez <mannynunezr@gmail.com>
2022-04-15 14:35:47 -0400

    added images link and updated schema to include image

--- models/characters.js
+++ models/characters.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 let Character = new Schema({
   name: { type: String, required: true },
+  image: { type: String },
   firstAppearance: { type: String },
   goodGuy: { type: Boolean },
   species: { type: String },
-  descritpions: { type: String },
+  description: { type: String },
 });
 
 export default mongoose.model("characters", Character);
